830947ca9883eba3f55761246dd093d0f27da128
Andrew Solan <andrewsolan@gmail.com>
2021-12-27 13:36:56 -0800

    feedback v1. bug fixes

--- src/components/AyogiSettings/AyogiFeedback.js
+++ src/components/AyogiSettings/AyogiFeedback.js
@@ -7,12 +7,14 @@ import {
   IonSelect,
   IonSelectOption,
   IonListHeader,
-  IonLabel
+  IonButton,
+  IonLabel,
+  IonTextarea
 } from "@ionic/react";
 import { textOutline, sunny } from "ionicons/icons";
 import { useForm } from "react-hook-form";
 
-import AyogiWisdom from "../AyogiWisdom/AyogiWisdom";
+//import AyogiWisdom from "../AyogiWisdom/AyogiWisdom";
 //import AyogiContext from '../context/AyogiContext';
 import "./AyogiFeedback.css";
 import constants from "../../store/constants";
@@ -21,40 +23,55 @@ const AyogiFeedback = (props) => {
   const { register, handleSubmit } = useForm();
   const [result, setResult] = useState("");
   const onSubmit = (data) => {
+    console.log('onSubmit', data);
     setResult(JSON.stringify(data))
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <IonList>
-        <IonItem>
+        <IonItem lines="full" className="ion-margin-end">
           <IonListHeader>
-            <IonLabel>Send Feedback</IonLabel>
+            <IonLabel><h1>Submit Feedback</h1></IonLabel>
           </IonListHeader>
         </IonItem>
-        <IonItem>
-          <IonInput ref={register("feedbackTitle")} placeholder="Feedback Title" />
+        <IonItem lines="full">
+          <IonLabel className="ion-margin-end">Summary</IonLabel>
+          <IonInput 
+            {...register("feedbackTitle", { required: true, maxLength: 100 })}
+            placeholder="Summary of Feedback" />
         </IonItem>
-        <IonItem>
-          <IonInput ref={register("feedbackDescription")} placeholder="Feedback Description" />
+        <IonItem lines="full">
+        <IonLabel className="ion-margin-end">Description</IonLabel>
+          <IonTextarea 
+            autoGrow="true" 
+            {...register("feedbackDescription", { required: true, maxLength: 2000 })}
+            // ref={register("feedbackDescription")} 
+            placeholder="Please describe" />
         </IonItem>
-        <IonItem>
-          <IonLabel>Feedback Area</IonLabel>
-          <IonSelect 
+        <IonItem lines="full">
+          <IonLabel>Area</IonLabel>
+          <IonSelect
             name="feedbackArea" 
-            ref={register("feedbackArea")}
+            {...register("feedbackArea", { required: false })}
             placeholder="Select One" 
             // onIonChange={e => setGender(e.detail.value)}
             >
-            <IonSelectOption value="female">Female</IonSelectOption>
-            <IonSelectOption value="male">Male</IonSelectOption>
+              <IonSelectOption value="chapters">Chapters</IonSelectOption>
+              <IonSelectOption value="book">Book</IonSelectOption>
+              <IonSelectOption value="shareQuote">Share Quote</IonSelectOption>
+              <IonSelectOption value="saveQuote">Save Quote</IonSelectOption>
+              <IonSelectOption value="search">Search</IonSelectOption>
+              <IonSelectOption value="quotes">Quotes</IonSelectOption>
+              <IonSelectOption value="Settings">Settings</IonSelectOption>
           </IonSelect>
         </IonItem>
-        <IonItem>
-          <input type="submit" />
+        <IonItem lines="full">
+          <IonButton slot="start" color="primary" type="submit">Submit</IonButton>
+          <IonButton slot="end" color="light  ">Cancel</IonButton>
         </IonItem>
-        <IonItem>
-          {result}
+        <IonItem lines="full">
+          {JSON.parse(JSON.stringify(result)) }
         </IonItem>
       </IonList>
     </form>
